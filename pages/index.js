8dc0a1a031a16d764b02a0eb110d4ba55e81feed
Diefferson Siqueira <60404007+DieffersonS@users.noreply.github.com>
2024-12-18 15:30:13 +0000

    Subindo arquivos da página

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+
+function Home(){
+   return <img src="https://w7.pngwing.com/pngs/334/490/png-transparent-capricorn-shura-saint-seiya-soldiers-soul-pegasus-seiya-shaka-aries-mu-capricorn.png" width='400px'></img>;
+   }
+
+
+
+export default Home;
