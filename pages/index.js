b292b400c86cd9bccbcda8d084d9bef7da703584
Diefferson Siqueira <60404007+DieffersonS@users.noreply.github.com>
2024-12-31 21:11:06 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-
-
-function Home(){
-   return <img src="https://w7.pngwing.com/pngs/334/490/png-transparent-capricorn-shura-saint-seiya-soldiers-soul-pegasus-seiya-shaka-aries-mu-capricorn.png" width='400px'></img>;
-   }
-
-
+const Home = () => {
+  return (
+    <img scr="https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"></img>
+  );
+};
 
 export default Home;
